1935da2d9a361a4822417e5f42fffa956fa39eed
Crunnie <34610490+Crunnie@users.noreply.github.com>
2017-12-22 12:20:58 +0100

    Create Utilities.cs

---
+++ Utilities.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+
+        public static string GetFormattedAlert(string key, params object[] parameter)
+        {
+            if (alerts.ContainsKey(key))
+            {
+                return String.Format(alerts[key], parameter);
+            }
+            return "";
+        }
+
+        public static string GetFormattedAlert(string key, object parameter)
+        {
+            return GetFormattedAlert(key, new object[] { parameter });
+        }
+
+        public static bool BotAdmin(SocketGuildUser user)
+        {
+            string targetRoleName = "Crunnie Admin";
+            var result = from r in user.Guild.Roles
+                         where r.Name == targetRoleName
+                         select r.Id;
+            ulong roleID = result.FirstOrDefault();
+            if (roleID == 0) return false;
+            var targetRole = user.Guild.GetRole(roleID);
+            return user.Roles.Contains(targetRole); // gonna use you for something soon ;)
+        }
+    }
+}
